3100a0942e98eae42dd4d632adc00cacd7d55dd3
Marko <marko.k.89@gmail.com>
2017-10-24 20:08:14 +0200

    part4

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react';
 import { Link, IndexLink } from 'react-router';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
-const Header = ({ loading }) => {
+const Header = () => {
     return (
         <Navbar inverse collapseOnSelect>
             <Navbar.Header>
@@ -38,27 +38,12 @@ const Header = ({ loading }) => {
                             About Page
                         </Link>
                     </NavItem>
-                    {/* <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                        <MenuItem eventKey={3.1}>Action</MenuItem>
-                        <MenuItem eventKey={3.2}>Another action</MenuItem>
-                        <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                        <MenuItem divider />
-                        <MenuItem eventKey={3.3}>Separated link</MenuItem>
-                        </NavDropdown>*/
-                    }
                 </Nav>
-                {/* <Nav pullRight>
-                    <NavItem eventKey={1} href="#">Link Right</NavItem>
-                    <NavItem eventKey={2} href="#">Link Right</NavItem>
-                </Nav>
-               */}
             </Navbar.Collapse>
         </Navbar>
     );
 };
 
-Header.propTypes = {
-    loading: PropTypes.bool.isRequired
-};
+
 
 export default Header;
---
+++ src/components/courses/CoursesGrid.js
@@ -0,0 +1,102 @@
+import React from 'react';
+
+class CoursesGrid extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Courses List</h1>
+        <br/>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-4">
+              <div className="panel panel-primary">
+                <div className="panel-heading">
+                  Course
+                </div>
+                <div className="panel-body">
+                  picture/text
+                </div>
+                <div className="panel-footer">
+                  Lesson
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-4">
+              <div className="panel panel-primary">
+                <div className="panel-heading">
+                  Course
+                </div>
+                <div className="panel-body">
+                  picture/text
+                </div>
+                <div className="panel-footer">
+                  Lesson
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-4">
+              <div className="panel panel-primary">
+                <div className="panel-heading">
+                  Course
+                </div>
+                <div className="panel-body">
+                  picture/text
+                </div>
+                <div className="panel-footer">
+                  Lesson
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-4">
+              <div className="panel panel-primary">
+                <div className="panel-heading">
+                  Course
+                </div>
+                <div className="panel-body">
+                  picture/text
+                </div>
+                <div className="panel-footer">
+                  Lesson
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-4">
+              <div className="panel panel-primary">
+                <div className="panel-heading">
+                  Course
+                </div>
+                <div className="panel-body">
+                  picture/text
+                </div>
+                <div className="panel-footer">
+                  Lesson
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-4">
+              <div className="panel panel-primary">
+                <div className="panel-heading">
+                  Course
+                </div>
+                <div className="panel-body">
+                  picture/text
+                </div>
+                <div className="panel-footer">
+                  Lesson
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default CoursesGrid;
