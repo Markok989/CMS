825eba5b18cb14bbc260577a22bf7e416d5d8364
Marko <marko.k.89@gmail.com>
2017-10-25 13:58:25 +0200

    part5

---
+++ src/components/courses/CoursesGridRow.js
@@ -0,0 +1,12 @@
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+
+class CoursesGridRow extends React.Component {
+    render() {
+        return (
+            <p>test</p>
+        );
+    }
+}
+
+export default CoursesGridRow;
